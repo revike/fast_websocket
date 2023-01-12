52307d684d878b4c708ae214be2d1d6181fe1a35
revike <revike@ya.ru>
2023-01-12 10:13:08 +0300

    start project

---
+++ utils/websocket_manager.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from starlette.websockets import WebSocket
+
+
+class ConnectionManager:
+
+    def __init__(self):
+        self.count = 0
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.count = 0
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, data, websocket: WebSocket,
+                                    count=False):
+        if count:
+            self.count += 1
+            data['count'] = self.count
+        await websocket.send_json(data)
