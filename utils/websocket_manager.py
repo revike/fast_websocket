57eda530aa8c0126a0793b5c8f6966581e170957
revike <revike@ya.ru>
2023-01-13 09:32:41 +0300

    update count

--- utils/websocket_manager.py
+++ utils/websocket_manager.py
@@ -6,7 +6,6 @@ from starlette.websockets import WebSocket
 class ConnectionManager:
 
     def __init__(self):
-        self.count = 0
         self.active_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
@@ -14,10 +13,8 @@ class ConnectionManager:
         self.active_connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
-        self.count = 0
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, data, websocket: WebSocket):
-        self.count += 1
-        data['count'] = self.count
+    @staticmethod
+    async def send_personal_message(data, websocket: WebSocket):
         await websocket.send_json(data)
